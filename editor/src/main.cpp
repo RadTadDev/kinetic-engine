558b2b3583e6e52671633b981b7aa80e742776a7
TadDiDio <taddidio@vt.edu>
2026-08-15 15:38:17 -0500

    compiles and links locally

---
+++ editor/src/main.cpp
@@ -0,0 +1,9 @@
+#include "Engine.h"
+
+int main() {
+
+	Engine engine;
+	engine.Print("Hello from the Editor!");
+	
+	return 0;
+}
---
+++ engine/src/Engine.cpp
@@ -0,0 +1,7 @@
+#include "Engine.h"
+#include "iostream"
+
+const void Engine::Print(const string& message)
+{
+	std::cout << message << std::endl;
+}
